d01ecb2018193ac5b62b0b94148c66e33f685bc2
AtheDev <athe.dev@gmail.com>
2020-05-12 19:40:36 +0200

    Formulaire

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,30 @@
 class EventsController < ApplicationController
+
   def index
+    @event = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new( 'admin_id' => session[:user_id],
+                        'title' => params[:title],
+                        'description' => params[:description],
+                        'location' => params[:location],
+                        'price' => params[:price],
+                        'start_date' => params[:start_date],
+                        'duration' => params[:duration])
+    # if @event.save
+    #   @event = Event.all
+    #   flash[:success] = ""
+    #   render :show
+    # end
+  end
+
 end
